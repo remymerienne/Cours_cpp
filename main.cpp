3178ce9439889c36cfb68415c86dc5b15f33d7ee
Rémy Merienne <remy.merienne-dev@outlook.fr>
2023-05-23 18:11:22 +0200

    add variables et saisie utilisateur

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 /*
   #1. Affichage console et sorties standards
   #2. Les variables
+  #3. Saisie utilisateur
 */
 
 /*
@@ -12,11 +13,14 @@
 */
 #include <iostream>
 
+#include <string>
+
 // Fonction principale qui contient notre programme en c++
 int main()
 {
 
   // #1. Affichage console et sorties standards
+  // ******************************************
 
   /*
     std::cout : affichage standard (en mémoire tampon)
@@ -34,7 +38,87 @@ int main()
   std::cout << "\nHello world !\n" << std::endl;
 
   // #2. Les variables
+  // *****************
 
-  // Fin de notre fonction principale
-  return 0;
+  /*
+    Les principaux suffixes :
+    -------------------------
+    u, U -> non signé
+    f, F -> flottant
+    l - L -> long int - Long double
+    ll, LL -> long lonf int  
+
+    Préfixes :
+    ----------
+    0b -> binaire
+    0 -> octal
+    0x -> héxadécimal
+  */
+
+  // Déclaration d'une variable booléenne par défaut
+  bool is_secure{};
+
+  // Déclaration d'une variable de type entier par défaut
+  float my_float{};
+
+  // Initialisation d'une variable de type int
+  int my_int{1'979l};
+
+  // Laisser le compilateur analyser le type de variable
+  auto mon_test{22};
+
+  // Affectation d'une variable
+  my_float = 1976.0f;
+
+  // Initialisation d'une variable string
+  std::string my_short_message{"Voici mon message court"};
+
+  const float MA_CONSTANTE_PI{3.14116f};
+
+  // Affichage des variables
+  std::cout << is_secure << std::endl;
+  std::cout << my_int << std::endl;
+  std::cout << my_float << std::endl;
+  std::cout << my_short_message << std::endl;
+  std::cout << MA_CONSTANTE_PI << std::endl;
+  std::cout << mon_test << std::endl;
+
+  using namespace std::literals;
+  auto some_string{"Bonjour le monde"s};
+  std::cout << some_string << std::endl;
+
+  // #3. Saisie utilisateur
+  // **********************
+
+  /*
+    Tests d'une entrée avec les méthodes de cin :
+    ---------------------------------------------
+    std::cout << "Good : " << std::cin.good() << std::endl;  
+    std::cout << "Fail : " << std::cin.fail() << std::endl;
+    std::cout << "Bad : " << std::cin.bad() << std::endl;
+    Si cin est dans un état d'erreur : std::cin.clear()
+
+    Voir aussi après :
+    ------------------
+    std::cin.ingnore()
+    std::ws
+  */
+
+  // Saisie d'un entier
+  std::cout << "Tapez un nombre entier : ";
+  int number;
+  std::cin >> number; // \n
+
+  // Capture du retour chariot (flush de sortie)
+  std::string mon_nom_complet;
+  std::getline(std::cin, mon_nom_complet);
+
+  // Saisie d'une chaine de caractères
+  std::cout << "Entrez votre nom complet : ";
+  std::getline(std::cin, mon_nom_complet);
+
+  // Affichage des saisies
+  std::cout << "Nombre saisi : " << number << std::endl;
+  std::cout << "Votre nom complet est : " << mon_nom_complet << std::endl;
+
 }
